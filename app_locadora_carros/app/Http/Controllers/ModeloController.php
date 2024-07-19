3231ac0b74cb47f30e9166b53c7d1435026e2eb4
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-07-19 10:10:48 -0300

    feat: :sparkles: Obtendo colunas específicas do relacionamento

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -19,11 +19,18 @@ class ModeloController extends Controller
     public function index(Request $request)
     {
         $modelos= array();
+        if($request->has('atributos_marca')){
+            $atributos_marca = $request->atributos_marca;
+            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
+        }else{
+            $modelos = $this->modelo->with('marca');
+        }
         if($request->has('atributos')){
             $atributos = $request->atributos;
-            $modelos = $this->modelo->selectRaw($atributos)->with('marca')->get();
+            $atributos_marca = $request->atributos_marca;
+            $modelos = $modelos->selectRaw($atributos)->get();
         }else{
-            $modelos = $this->modelo->with('marca')->get();
+            $modelos = $modelos->get();
         }
         return response()->json($modelos, 200);
         //all() -> criando um obj de constulta + get() = collection
