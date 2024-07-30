7b6daa806223dca3145125e76504642748fdc1b1
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-07-30 09:14:23 -0300

    feat: :sparkles: Implementando AbstractRepository

--- app_locadora_carros/app/Repositories/ModeloRepository.php
+++ app_locadora_carros/app/Repositories/ModeloRepository.php
@@ -1,33 +1,7 @@
 <?php
 
     namespace App\Repositories;
+    class ModeloRepository extends AbstractRepository{
 
-    use Illuminate\Database\Eloquent\Model;
-
-    class ModeloRepository{
-        public function __construct(Model $model)
-        {
-            $this->model = $model;
-        }
-
-        public function SelectAtributosRegistrosRelacionados($atributos){
-            $this->model = $this->model->with($atributos);
-        }
-
-        public function filtro($filtros){
-            $filtros = explode(';', $filtros);
-            foreach($filtros as $condicao){
-                $c = explode(':', $condicao);
-                $this->model = $this->model->where($c[0], $c[1], $c[2]);
-            }
-        }
-
-        public function selectAtributos($atributos){
-            $this->model = $this->model->selectRaw($atributos);
-        }
-
-        public function getResultado(){
-            return $this->model->get();
-        }
     }
 ?>
