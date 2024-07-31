494c574db6643e67014c1c425b26330bfc06ddf5
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-07-31 09:28:03 -0300

    feat: :sparkles: Implementando resources e repository de Cliente

--- app_locadora_carros/app/Http/Controllers/ClienteController.php
+++ app_locadora_carros/app/Http/Controllers/ClienteController.php
@@ -3,19 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
-use App\Http\Requests\StoreClienteRequest;
-use App\Http\Requests\UpdateClienteRequest;
+use App\Repositories\ClienteRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repositories\CarroRepository;
 
 class ClienteController extends Controller
 {
+    public function __construct(Cliente $cliente)
+    {
+        $this->cliente = $cliente;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new clienteRepository($this->cliente);
+        if($request->has('filtro')){
+            $clienteRepository->filtro($request->filtro);
+        }
+        if($request->has('atributos')){
+            $clienteRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($clienteRepository->getResultado(), 200);
     }
 
 
@@ -25,9 +38,12 @@ class ClienteController extends Controller
      * @param  \App\Http\Requests\StoreClienteRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreClienteRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules());
+        $cliente = $this->cliente->create(['nome'=> $request->nome]);
+
+        return response()->json($cliente, 201);
     }
 
     /**
@@ -36,9 +52,13 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null){
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+        return response()->json($cliente, 200);
     }
 
 
@@ -49,9 +69,29 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateClienteRequest $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null){
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+        if($request->method() === 'PATCH'){
+            $regrasDinamicas = array();
+            // Percorrendo todas as regras definidas no model
+            foreach($cliente->rules() as $input => $regra){
+                // Coletar apenas as regras aplicáveis  aos parametros parcias da requisição
+                if(array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas);
+        }else{
+            $request->validate($cliente->rules());
+        }
+        //preencher o objeto $cliente com os dados do request
+        $cliente->fill($request->all());
+        $cliente->save();
+        return response()->json($cliente, 200);
     }
 
     /**
@@ -60,8 +100,14 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $cliente= $this->cliente->find($id);
+        if($cliente === null){
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404) ;
+        }
+        // Remove o arquivo caso ele exista
+        $cliente->delete();
+        return response()->json(['msg' => 'O cliente foi removida com sucesso'], 200);
     }
 }
