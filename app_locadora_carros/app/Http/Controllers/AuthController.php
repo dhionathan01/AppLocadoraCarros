236d71cc5e0b635669525333913d77c524917532
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-08 09:58:22 -0300

    feat: :sparkles: Implementando a autenticação e geração do JWT

--- app_locadora_carros/app/Http/Controllers/AuthController.php
+++ app_locadora_carros/app/Http/Controllers/AuthController.php
@@ -6,7 +6,19 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        $credenciais = ($request->all(['email', 'password']));
+        // autenticação email e senha
+        $token = auth('api')->attempt($credenciais);
+        if($token){
+            return response()->json(['token' => $token]);
+        }else{
+            return response()->json(['erro' => 'Usuário ou senha inválido'], 403);
+            // 401 = Unauthorized -> não autorizado.
+            // 403 = forbidden -> proibido (login inválido).
+
+        }
+        // retornar um Json Web Token
         return 'login';
     }
     public function logout(){
