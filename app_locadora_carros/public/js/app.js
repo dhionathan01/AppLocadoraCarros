84f71832fea66f20a9fe2b4c589becc9aa21a712
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-08-17 10:40:44 -0300

    feat: :sparkles: Criando o card para listagem de registros marca

--- app_locadora_carros/public/js/app.js
+++ app_locadora_carros/public/js/app.js
@@ -5464,7 +5464,10 @@ var render = function render() {
   }, [_c("div", {
     staticClass: "col-md-8"
   }, [_c("div", {
-    staticClass: "card"
+    staticClass: "card mb-3",
+    attrs: {
+      id: "card_busca"
+    }
   }, [_c("div", {
     staticClass: "card-header"
   }, [_vm._v("Busca de marcas")]), _vm._v(" "), _c("div", {
@@ -5505,7 +5508,7 @@ var render = function render() {
       "aria-describedby": "nomeHelp",
       placeholder: "Nome da marca"
     }
-  })])], 1)])]), _vm._v(" "), _vm._m(0)])])])]);
+  })])], 1)])]), _vm._v(" "), _vm._m(0)]), _vm._v(" "), _vm._m(1)])])]);
 };
 var staticRenderFns = [function () {
   var _vm = this,
@@ -5519,6 +5522,61 @@ var staticRenderFns = [function () {
       type: "submit"
     }
   }, [_vm._v("Pesquisar")])]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c,
+    _setup = _vm._self._setupProxy;
+  return _c("div", {
+    staticClass: "card",
+    attrs: {
+      id: "listagem_marcas"
+    }
+  }, [_c("div", {
+    staticClass: "card-header"
+  }, [_vm._v("Relação de marcas")]), _vm._v(" "), _c("div", {
+    staticClass: "card-body"
+  }, [_c("table", {
+    staticClass: "table table-hover"
+  }, [_c("thead", [_c("tr", [_c("th", {
+    attrs: {
+      scope: "col"
+    }
+  }, [_vm._v("#")]), _vm._v(" "), _c("th", {
+    attrs: {
+      scope: "col"
+    }
+  }, [_vm._v("First")]), _vm._v(" "), _c("th", {
+    attrs: {
+      scope: "col"
+    }
+  }, [_vm._v("Last")]), _vm._v(" "), _c("th", {
+    attrs: {
+      scope: "col"
+    }
+  }, [_vm._v("Handle")])])]), _vm._v(" "), _c("tbody", [_c("tr", [_c("th", {
+    attrs: {
+      scope: "row"
+    }
+  }, [_vm._v("1")]), _vm._v(" "), _c("td", [_vm._v("Mark")]), _vm._v(" "), _c("td", [_vm._v("Otto")]), _vm._v(" "), _c("td", [_vm._v("@mdo")])]), _vm._v(" "), _c("tr", [_c("th", {
+    attrs: {
+      scope: "row"
+    }
+  }, [_vm._v("2")]), _vm._v(" "), _c("td", [_vm._v("Jacob")]), _vm._v(" "), _c("td", [_vm._v("Thornton")]), _vm._v(" "), _c("td", [_vm._v("@fat")])]), _vm._v(" "), _c("tr", [_c("th", {
+    attrs: {
+      scope: "row"
+    }
+  }, [_vm._v("3")]), _vm._v(" "), _c("td", {
+    attrs: {
+      colspan: "2"
+    }
+  }, [_vm._v("Larry the Bird")]), _vm._v(" "), _c("td", [_vm._v("@twitter")])])])])]), _vm._v(" "), _c("div", {
+    staticClass: "card-footer"
+  }, [_c("button", {
+    staticClass: "btn btn-primary btn-sm float-end",
+    attrs: {
+      type: "button"
+    }
+  }, [_vm._v("Adicionar")])])]);
 }];
 render._withStripped = true;
 
--- app_locadora_carros/resources/js/components/Marcas.vue
+++ app_locadora_carros/resources/js/components/Marcas.vue
@@ -2,21 +2,23 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <!--  Início do card de busca -->
+                <div class="card mb-3" id="card_busca">
                     <div class="card-header">Busca de marcas</div>
-
                     <div class="card-body">
                         <div class="row">
                             <div class="col mb-3">
-                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o id da marca">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                    texto-ajuda="Opcional. Informe o id da marca">
                                     <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
-                                    placeholder="ID">
+                                        placeholder="ID">
                                 </input-container-component>
                             </div>
                             <div class="col mb-3">
-                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
+                                    texto-ajuda="Opcional. Informe o nome da marca">
                                     <input type="number" class="form-control" id="inputId" aria-describedby="nomeHelp"
-                                    placeholder="Nome da marca">
+                                        placeholder="Nome da marca">
                                 </input-container-component>
                             </div>
                         </div>
@@ -25,6 +27,46 @@
                         <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
                     </div>
                 </div>
+                <!-- Fim do card de busca -->
+                <!-- Inicio do card de listagem de marcas -->
+                <div class="card" id="listagem_marcas">
+                    <div class="card-header">Relação de marcas</div>
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                    </div>
+                </div>
+                <!--  Fim do card de listagem de marcas -->
             </div>
         </div>
     </div>
